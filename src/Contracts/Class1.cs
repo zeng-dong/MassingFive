8a483c0a60e19b1b93f36019c8fd5213fb8ec88c
Dong Zeng <dong@freshbyte.com>
2021-11-30 20:19:01 -0600

    added Contracts project

---
+++ src/Contracts/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contracts
+{
+    public interface SubmitOrder
+    {
+        Guid OrderId { get; set; }
+        DateTime TimeStamp { get; set; }
+        string CustomerNumber { get; set; }
+    }
+}
